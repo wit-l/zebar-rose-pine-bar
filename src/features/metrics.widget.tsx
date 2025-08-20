c7db4be258475654ffd3c91087c39850af662341
thearturca <arti.1046@gmail.com>
2025-08-20 17:38:45 +0300

    feat(motion): adds animation for metrics feature

--- src/features/metrics.widget.tsx
+++ src/features/metrics.widget.tsx
@@ -1,46 +1,67 @@
 import { useProviders } from "@providers/index";
 import { GroupItem } from "@components/group.component";
+import { animate } from "motion";
+import { createEffect } from "solid-js";
+import { useMotionValue } from "@/motion/hooks";
 
 export function MetricsWidget() {
   const providers = useProviders();
+  const cpuUsage = useMotionValue(0);
+  const memoryUsage = useMotionValue(0);
+  const weather = useMotionValue(0);
+
+  createEffect<ReturnType<typeof animate>>((prev) => {
+    prev?.stop();
+    const usage = providers.cpu?.usage;
+    return animate(cpuUsage.raw, usage || 0, {
+      duration: 1,
+      ease: "circOut",
+    });
+  });
+
+  createEffect<ReturnType<typeof animate>>((prev) => {
+    prev?.stop();
+    const usage = providers.memory?.usage;
+    return animate(memoryUsage.raw, usage || 0, {
+      duration: 1,
+      ease: "circOut",
+    });
+  });
+
+  createEffect<ReturnType<typeof animate>>((prev) => {
+    prev?.stop();
+    const celsiusTemp = providers.weather?.celsiusTemp;
+    return animate(weather.raw, celsiusTemp || 0, {
+      duration: 1,
+      ease: "circOut",
+    });
+  });
 
   return (
     <GroupItem>
       <i class="text-2xl text-rose-pine-rose"></i>
-      {Math.round(providers.cpu?.usage || 0).toLocaleString(undefined, {})}%
+      {Math.round(cpuUsage.get()).toLocaleString(undefined, {})}%
       <i class="text-2xl text-rose-pine-pine"></i>
-      {Math.round(providers.memory?.usage || 0).toLocaleString(undefined, {})}%
+      {Math.round(memoryUsage.get()).toLocaleString(undefined, {})}%
       <i
         class="text-2xl transition-colors"
         classList={{
-          "text-rose-pine-foam":
-            providers.weather?.celsiusTemp !== undefined &&
-            providers.weather?.celsiusTemp <= -20,
+          "text-rose-pine-foam": weather.get() <= -20,
 
-          "text-rose-pine-pine":
-            providers.weather?.celsiusTemp !== undefined &&
-            providers.weather?.celsiusTemp <= -10,
+          "text-rose-pine-pine": weather.get() <= -10,
 
-          "text-rose-pine-iris":
-            providers.weather?.celsiusTemp !== undefined &&
-            providers.weather?.celsiusTemp <= 5,
+          "text-rose-pine-iris": weather.get() <= 5,
 
-          "text-rose-pine-rose":
-            providers.weather?.celsiusTemp !== undefined &&
-            providers.weather?.celsiusTemp <= 10,
+          "text-rose-pine-rose": weather.get() <= 10,
 
-          "text-rose-pine-gold":
-            providers.weather?.celsiusTemp !== undefined &&
-            providers.weather?.celsiusTemp <= 25,
+          "text-rose-pine-gold": weather.get() <= 25,
 
-          "text-rose-pine-love":
-            providers.weather?.celsiusTemp !== undefined &&
-            providers.weather?.celsiusTemp >= 25,
+          "text-rose-pine-love": weather.get() >= 25,
         }}
       >
         󰖙
       </i>
-      {Math.round(providers.weather?.celsiusTemp ?? 0)}°
+      {Math.round(weather.get())}°
     </GroupItem>
   );
 }
